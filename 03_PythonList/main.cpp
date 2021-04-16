cd80cf9f90bf3e8509d693f3ed10cb18ec7f8916
Jaybee18 <mister.jaybee18@gmail.com>
2021-04-16 09:19:23 +0200

    created and started Python list

--- 03_PythonList/main.cpp
+++ 03_PythonList/main.cpp
@@ -1,6 +1,9 @@
 #include "plist.h"
+#include <list>
 
 int main(){
     plist<float> a;
+    a.append(0.01f);
+    a.append(0.02f);
     return 0;
 }
--- 03_PythonList/plist.h
+++ 03_PythonList/plist.h
@@ -1,11 +1,18 @@
-template <class contentType> class obj
+#include <cstddef>
+#include <iostream>
+
+template <class contentType>
+class obj
 {
 public:
-    contentType a;
-    next
-    obj()
+    contentType value;
+    obj<contentType>* next;
+    bool isEmpty = true;
+    obj() {}
+    obj(contentType pValue)
     {
-
+        value = pValue;
+        isEmpty = true;
     }
 };
 
@@ -13,9 +20,37 @@ template <class contentType>
 class plist
 {
 public:
-    obj<contentType> head;
+    obj<contentType> first;
+    obj<contentType> current;
     plist()
     {
+    }
 
+    void toFirst()
+    {
+        current = first;
+    }
+
+    void next()
+    {
+        current = *(current.next);
+    }
+
+    void append(contentType object)
+    {
+        if (first.isEmpty)
+        {
+            first = obj<contentType>(object);
+            return;
+        }
+        toFirst();
+        int safetyCounter = 0;
+        while (!(*current.next).isEmpty)
+        {
+            if( safetyCounter > 100 ){std::cout << "while true loop!" << std::endl; break;}
+            safetyCounter++;
+            next();
+        }
+        current.next = new obj<contentType>(object);
     }
 };
