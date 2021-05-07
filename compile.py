72844e7eaccfe4baeb6b061cfee9480ba862faf6
Jaybee18 <mister.jaybee18@gmail.com>
2021-05-07 19:04:27 +0200

    compiling script

---
+++ compile.py
@@ -0,0 +1,8 @@
+import os, sys
+# g++ 08_NEAT/main.cpp -o 08_NEAT/main.exe -I"C:\MinGW\include" -L"C:\MinGW\lib" -lfreeglut -lopengl32 && 08_NEAT\main.exe
+if __name__ == "__main__":
+    projectId = sys.argv[1]
+    folders = os.listdir()
+    ids = list(map(lambda x: str(x)[:2], folders))
+    indexOfFolder = ids.index(projectId)
+    os.system(f"g++ {folders[indexOfFolder]}/main.cpp -o {folders[indexOfFolder]}/main.exe -I\"C:\\MinGW\\include\" -L\"C:\\MinGW\\lib\" -lfreeglut -lopengl32 && {folders[indexOfFolder]}\\main.exe");
